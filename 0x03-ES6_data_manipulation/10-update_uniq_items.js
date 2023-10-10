21fdfd477ba5c426b559a0afa591f8c30c58ea4d
Distinctsy <oluwaseyiomi@gmail.com>
2023-10-10 12:50:31 +0100

    corrections

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,13 @@
-export default function updateUniqueItems(items) {
+const updateUniqueItems = (items) => {
   if (!(items instanceof Map)) {
     throw new Error('Cannot process');
   }
+  for (const [k, v] of items.entries()) {
+    if (v === 1) {
+      items.set(k, 100);
+    }
+  }
+  return items;
+};
+
+export default updateUniqueItems;
